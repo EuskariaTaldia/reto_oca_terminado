0fd0e290a5e59f7cad703f4a8761e9fda201780a
ik_2dw3 <ik_2dw3@H015110.NET>
2020-01-14 12:03:05 +0100

    tabla y peticiones

--- resources/views/peticion/crear.blade.php
+++ resources/views/peticion/crear.blade.php
@@ -2,12 +2,32 @@
 
 @section('content')
 
-    <h2>Formulario para la creacion de una nueva mujer:<h2>
+    <h2>Formulario para la creacion de una nueva mujer:</h2>
 
-    <form action="#" target="" method="post" name="formDatosPersonales">
 
-        <label for="nombre">Nombre y apellido</label>
-        <input type="text" name="nombre" id="nombre" placeholder="Escribe tu nombre"/>
+<!--
+    {!! Form::open(['action' => ['peticionController@getPeticion'] 'method' => 'POST']) !!}
+
+    {{ csrf_field() }}
+
+    {{Form::bstext('nombreApellido')}}
+    {{Form::bstext('subArea')}}
+    {{Form::bstext('zonaGeografica')}}
+    {{Form::bstext('foto')}}
+    {{Form::bstext('fecha')}}
+    {{Form::bstext('mensaje')}}
+    {{Form::bstext('URL')}}
+    {{Form::submit('enviar')}}
+
+    {!! Form::close()!!}-->
+
+
+
+    <!--form action="" method="GET" name="formDatosPersonales">
+
+        <label for="nombreApellido">Nombre y apellido</label>
+        <input type="text" name="nombreApellido" id="nombreApellido" placeholder="Escribe tu nombre"/>
+
 
         <label for="subArea">sub Area</label>
         <input type="text" name="subArea" id="subArea" placeholder="Area de trabajo"/>
@@ -16,10 +36,10 @@
         <input type="text" name="zonaGeografica" id="zonaGeografica" placeholder="de donde es"/>
 
         <label for="foto">foto</label>
-        <input type="file" name="foto" id="foto" placeholder="foto" required />
+        <input type="file" name="foto" id="foto" placeholder="foto"  />
 
         <label for="fecha">nacimimento-fallecimiento</label>
-        <input type ="date" name="fechas" id="fechas" placeholder="ej: 1950-2005"/><br>
+        <input type ="text" name="fechas" id="fechas"  placeholder="ej: 1909-2005"/><br>
 
         <label for="mensaje">información</label>
         <textarea name="mensaje" for="mensaje" placeholder="describe brevemente en menos de 300 carácteres" maxlength="300"></textarea>
@@ -29,6 +49,15 @@
 
         <input type="submit" name="enviar" value="enviar datos"/>
 
-    </form>
+    </!--form -->
+
+
+
+
+
+
+
+
+
 
 @stop
--- resources/views/peticion/tabla.blade.php
+++ resources/views/peticion/tabla.blade.php
@@ -2,6 +2,49 @@
 
 @section('content')
 
-    <h2>Peticiones sobre nuevas mujeres: <h2>
+<div id="settingsTable" class="hoc clear">
+  <h2>Peticiones sobre nuevas mujeres: <h2>
+    <table>
+        <tr>
+            <th>nombre apellido</th>
+            <th>subarea</th>
+            <th>zonaGeografica</th>
+            <th>foto</th>
+            <th>mensaje</th>
+            <th>fecha</th>
+            <th>url</th>
 
+        </tr>
+
+        <tr>
+
+            @foreach ($arraypeticiones as $peticion)
+
+            <td id="nombreApellido"> <input class="inputSetting" type="text" id="nombreApellido" name="nombreApellido" value="{{$peticion["nombreApellido"]}}" readonly></td>
+            <td id="subArea"> <input class="inputSetting " type="text" name="subArea" value="{{$peticion["subArea"]}}" readonly></td>
+            <td id="zonaGeografica"> <input class="inputSetting " type="text" name="zonaGeografica" value="{{$peticion["zonaGeografica"]}}" readonly></td>
+            <td id="foto"> <input class="inputSetting " type="text" name="foto" value="{{$peticion["foto"]}}" readonly></td>
+            <td id="fecha"> <input class="inputSetting " type="text" name="fecha" value="{{$peticion["fecha"]}}" readonly></td>
+            <td id="mensaje"> <input class="inputSetting " type="text" name="mensaje" value="{{$peticion["mensaje"]}}" readonly></td>
+            <td id="enlace"> <input class="inputSetting " type="text" name="enlace" value="{{$peticion["enlace"]}}" readonly></td>
+            <td>
+                <!-- BOTONES DE LAS OPCIONES CORRESPONDIENTES -->
+                <div class="commentsIcon">
+                    <button type="submit" name="btnDeleteUser" class="btn btn-dark">
+                      aceptar
+                    </button>
+
+                    <button type="button" name="btnEditUser" class="btn btn-dark">
+                        denegar
+                    </button>
+                </div>
+            </td>
+
+
+
+
+        </tr>
+        @endforeach
+
+</div>
 @stop
